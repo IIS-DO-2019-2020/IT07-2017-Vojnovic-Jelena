526b56d99ed729978f6489a14815042186d422d1
TheJelena <jelena.vojnovic2812@gmail.com>
2021-09-19 00:06:31 +0200

    Shapes added

---
+++ DizajnerskiObrasci/src/shapes/Shape.java
@@ -0,0 +1,38 @@
+package shapes;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public abstract class Shape implements Moveable, Comparable<Object> {
+	
+	private boolean selected = false;
+	private Color edgeColor;
+	
+	public Shape() {
+		
+	}
+	
+	public Shape(boolean selected) {
+		this.selected=selected;
+	}
+	
+	public abstract boolean contains(int x, int y);
+	public abstract void draw(Graphics g);
+	
+	public boolean isSelected() {
+		return selected;
+	}
+	
+	public void setSelected(boolean selected) {
+		this.selected=selected;
+	}
+	
+	public Color getEdgeColor() {
+		return edgeColor;
+	}
+	
+	public void setEdgeColor(Color edgeColor) {
+		this.edgeColor=edgeColor;
+	}
+
+}
